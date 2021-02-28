dabdf5184b8398253e60e51e366f05656373bdeb
DESKTOP-4M4LOPF\lucas <mccreadl@sheridancollege.ca>
2021-02-28 00:17:12 -0500

    Completed the animations for the stick man

--- Assignment2/Assets/StickMove.cs
+++ Assignment2/Assets/StickMove.cs
@@ -7,6 +7,7 @@ public class StickMove : MonoBehaviour
     //bool lookR = false;
     public float speed = 0f;
     Vector3 screenPos;
+    public Animator animator;
 
 
     // Start is called before the first frame update
@@ -18,20 +19,29 @@ public class StickMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+
         Vector3 position = transform.position;
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = Camera.main.nearClipPlane;
         screenPos = Camera.main.ScreenToWorldPoint(mousePos);
 
 
-        if (screenPos.x > transform.position.x && Mathf.Abs(screenPos.x) < 9.5 && Mathf.Abs(screenPos.y) < 5.5 && transform.position.x > -8.2) {
-            
+        if (screenPos.x > transform.position.x && Mathf.Abs(screenPos.x) < 9.5 && Mathf.Abs(screenPos.y) < 5.5 && transform.position.x > -8.2)
+        {
+
             speed = -5f;
+
 
             transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
 
             transform.rotation = Quaternion.Euler(0, 0, 0);
 
+        }
+        else if (Mathf.Abs(screenPos.x) > 9.5 || Mathf.Abs(screenPos.y) > 5.5)
+        {
+            speed = 0f;
         }
 
         if (screenPos.x < transform.position.x && Mathf.Abs(screenPos.x) < 9.5 && Mathf.Abs(screenPos.y) < 5.5 && transform.position.x < 8.2)
@@ -44,9 +54,12 @@ public class StickMove : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 180, 0);
 
         }
+        else if (Mathf.Abs(screenPos.x) > 9.5 || Mathf.Abs(screenPos.y) > 5.5)
+        {
+            speed = 0f;
+        }
 
-
-
+        animator.SetFloat("Speed", speed);
 
 
 
