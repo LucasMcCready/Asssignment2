ac9d98866ddb9414529537852646dcf71486cd49
DESKTOP-4M4LOPF\lucas <mccreadl@sheridancollege.ca>
2021-02-27 22:41:51 -0500

    Added assets to project

---
+++ Assignment2/Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 screenPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        screenPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position = screenPos;
+        if (Input.GetKey(KeyCode.Mouse0)) {
+
+            Debug.Log(screenPos);
+        
+        }
+
+    }
+}
---
+++ Assignment2/Assets/StickMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickMove : MonoBehaviour
+{
+    bool lookR = false;
+    float speed = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
+
+        /*if (Input.GetKey(KeyCode.Mouse0)) {
+
+            if (lookR == false)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                lookR = true;
+            }
+            else {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                lookR = false;
+            }
+        
+
+        }
+        */
+
+    }
+}
