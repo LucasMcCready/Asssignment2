70eedf6cb9bae4675991416e0efc536b384e5f17
DESKTOP-4M4LOPF\lucas <mccreadl@sheridancollege.ca>
2021-02-27 23:46:59 -0500

    The stickman now moves away from the knife

--- Assignment2/Assets/StickMove.cs
+++ Assignment2/Assets/StickMove.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class StickMove : MonoBehaviour
 {
-    bool lookR = false;
-    float speed = 0f;
+    //bool lookR = false;
+    public float speed = 0f;
+    Vector3 screenPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 position = transform.position;
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        screenPos = Camera.main.ScreenToWorldPoint(mousePos);
 
-        transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
+
+        if (screenPos.x > transform.position.x && Mathf.Abs(screenPos.x) < 9.5 && Mathf.Abs(screenPos.y) < 5.5 && transform.position.x > -8.2) {
+            
+            speed = -5f;
+
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
+
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        }
+
+        if (screenPos.x < transform.position.x && Mathf.Abs(screenPos.x) < 9.5 && Mathf.Abs(screenPos.y) < 5.5 && transform.position.x < 8.2)
+        {
+
+            speed = 5f;
+
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0);
+
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        }
+
+
+
+
+
 
         /*if (Input.GetKey(KeyCode.Mouse0)) {
 
